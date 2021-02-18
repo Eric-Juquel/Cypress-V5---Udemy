032b2a08e574934a482edd2b261823d083aae9b8
Eric J <ejuquel@yahoo.fr>
2021-02-18 17:05:59 +0100

    web security

--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -2,7 +2,9 @@
 
 describe("Test Contact Us form via WebdriverUni", () => {
   it("Should be able to submit a successfull submition via contactUs form", () => {
-    cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+    //cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+    cy.visit("http://www.webdriveruniversity.com")
+    cy.get('#contact-us').invoke('removeAttr', 'target').click({force: true})
     cy.document().should("have.property", "charset").and("eq", "UTF-8");
     cy.title().should("include", "WebDriver | Contact Us");
     cy.url().should("include", "contactus");
@@ -16,7 +18,9 @@ describe("Test Contact Us form via WebdriverUni", () => {
   });
 
   it("Should not be able to submit a successfull submitionvia contactUs form as all fields are required", () => {
-    cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+    //cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+    cy.visit("http://www.webdriveruniversity.com")
+    cy.get('#contact-us').invoke('removeAttr', 'target').click({force: true})
 
     cy.get('[name="first_name"]').type("John");
     // cy.get('[name="last_name"]').type("Doe")
@@ -27,7 +31,10 @@ describe("Test Contact Us form via WebdriverUni", () => {
   });
 
   it("Should not be able to submit a successfull submition via contactUs form as email should be valid", () => {
-    cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+    //cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+    cy.visit("http://www.webdriveruniversity.com")
+    cy.get('#contact-us').invoke('removeAttr', 'target').click({force: true})
+
     cy.get('[name="first_name"]').type("John");
     cy.get('[name="last_name"]').type("Doe");
     cy.get('[name="email"]').type("john@example");
@@ -37,7 +44,10 @@ describe("Test Contact Us form via WebdriverUni", () => {
   });
 
   it("Should be able to reset the form  ", () => {
-    cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+    //cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+    cy.visit("http://www.webdriveruniversity.com")
+    cy.get('#contact-us').invoke('removeAttr', 'target').click({force: true})
+    
     // cy.get("#contact-us").click({force:true});
     cy.get('[name="first_name"]').type("John");
     cy.get('[name="last_name"]').type("Doe");
