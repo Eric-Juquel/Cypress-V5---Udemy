a6e29b9df03fb3a72520df44063758352a8fa9f8
Eric J <ejuquel@yahoo.fr>
2021-02-18 19:56:27 +0100

    checkbox

--- cypress/integration/webdriver-uni/iframe.js
+++ cypress/integration/webdriver-uni/iframe.js
@@ -6,19 +6,21 @@ describe("Handling IFrame & Modals", () => {
     cy.get("#iframe ").invoke("removeAttr", "target").click({ force: true });
     cy.url().should("include", "IFrame");
 
-    cy.get('#frame').then($iframe => {
-        const body = $iframe.contents().find('body')
-        cy.wrap(body).as('iframe')
-    })
+    cy.get("#frame").then(($iframe) => {
+      const body = $iframe.contents().find("body");
+      cy.wrap(body).as("iframe");
+    });
 
-    cy.get('@iframe').find('#button-find-out-more').click()
-    cy.get('@iframe').find('#myModal').as('modal')
+    cy.get("@iframe").find("#button-find-out-more").click();
+    cy.get("@iframe").find("#myModal").as("modal");
 
-    cy.get('@modal').should(($expectedText) => {
-        const text = $expectedText.text()
-        expect(text).to.include('Welcome to webdriveruniversity.com we sell a wide range of electrical goods such as laptops')
-    })
+    cy.get("@modal").should(($expectedText) => {
+      const text = $expectedText.text();
+      expect(text).to.include(
+        "Welcome to webdriveruniversity.com we sell a wide range of electrical goods such as laptops"
+      );
+    });
 
-    cy.get('@modal').contains('Close').click()
+    cy.get("@modal").contains("Close").click();
   });
 });
